ffef960ce9ee70a2e781a34ff5d068656a0124d6
vestease1972 <vestease1972@gmail.com>
2025-12-28 22:50:55 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,11 +1,9 @@
 import cloudinary from "cloudinary";
 
-const { v2 } = cloudinary;
-
-v2.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default v2;
+export default cloudinary; // 🔴 FULL MODULE, NOT v2
