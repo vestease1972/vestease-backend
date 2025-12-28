0445a3707bee14112a1f0039b4ec2c70197daf28
vestease1972 <vestease1972@gmail.com>
2025-12-28 22:24:46 +0530

    Delete backend directory

--- backend/routes/coupon.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { validateCoupon } from "../controllers/coupon.controller.js";
-
-const router = express.Router();
-
-router.post("/validate", validateCoupon);
-
-export default router;
